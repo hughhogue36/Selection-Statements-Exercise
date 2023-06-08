b4cc52d7a204b1adc4f5f1cc885b51ca0fff5c92
hughhogue36 <hvh18236@gmail.com>
2023-06-08 17:19:36 -0500

    Finished exercise: Selection Statements

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,74 @@
-﻿namespace SelectionStatementExercise
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //var r = new Random();
+            //var favNumber = r.Next(1, 1000);
+            //int userInput;
+            //var counter = 0;
+
+            //do
+            //{
+
+            //    Console.WriteLine("Pick a number from 1 to 1000.");
+            //    userInput = int.Parse(Console.ReadLine());
+
+            //    if (userInput < favNumber)
+            //    {
+            //        Console.WriteLine($"{userInput} is too low.");
+            //    }
+            //    else if (userInput > favNumber)
+            //    {
+            //        Console.WriteLine($"{userInput} is too high.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"You guessed it!");
+            //    }
+
+            //    counter++;
+            //    Console.WriteLine($"You have guessed {counter} time(s)");
+
+            //} while (userInput != favNumber);
+
+            Console.WriteLine("What is your favorite subject?");
+
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                case "geometry":
+                    Console.WriteLine("Math is the worst!");
+                    break;
+                case "science":
+                case "biology":
+                    Console.WriteLine("Science is pretty cool!");
+                    break;
+                case "PE":
+                case "phys ed":
+                case "physical education":
+                    Console.WriteLine("PE is the most fun!");
+                    break;
+                case "history":
+                    Console.WriteLine("History is old news.");
+                    break;
+                case "english":
+                    Console.WriteLine("English is important.");
+                    break;
+                default:
+                    Console.WriteLine("I don't know that subject.");
+                    break;
+            }
+
+
+
+
+                    
         }
     }
 }
